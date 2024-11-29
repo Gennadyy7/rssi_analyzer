b7e31cc3d5fb6542808538b0c0f0f0f71df46c8c
gena <pprom5573@gmail.com>
2024-11-29 16:28:42 +0300

    Исправил отслеживание файлов для либы

--- utils/get_ifaces.py
+++ utils/get_ifaces.py
@@ -6,6 +6,11 @@ class IfacesProvider:
 
     @classmethod
     def get_ifaces(cls):
-        give_rights()
-        interfaces = cls.WIFI.interfaces()[1:]
-        return interfaces
+        while True:
+            give_rights()
+            try:
+                interfaces = cls.WIFI.interfaces()[1:]
+            except FileNotFoundError as err:
+                print('Либа не справилась с отслеживанием файлов для адаптеров', err)
+                continue
+            return interfaces
